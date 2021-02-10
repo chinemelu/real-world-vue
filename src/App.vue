e236685fd1edd52a6033df4c8ad4cba7dbdd3598
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2021-02-10 04:25:10 +0100

    create base select component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <navbar></navbar>
     <NotificationContainer />
-    <router-view :key="$route.fullPath"/>
+    <router-view :key="$route.fullPath" />
   </div>
 </template>
 
